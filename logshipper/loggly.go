649561a97f6e3b17d4f7b77f790895d3b0017c7a
Girish Ranganathan <girish.rranganathan@gmail.com>
2020-03-11 20:38:53 -0400

    - adopted and extended kail to ship logs to loggly and papertrail - updated README - updated Dockerfile - updated yamls with a service account and a secret for holding the papertrail and loggly info - added a Makefile for static checks, build and docker

---
+++ logshipper/loggly.go
@@ -0,0 +1,33 @@
+package logshipper
+
+import (
+	"github.com/boz/kail"
+	loggly "github.com/segmentio/go-loggly"
+)
+
+type LogglyShipper struct {
+	logglyClient *loggly.Client
+}
+
+func CreateLogglyShipper(token string) *LogglyShipper {
+	return &LogglyShipper{
+		logglyClient: loggly.New(token),
+	}
+}
+
+func (l *LogglyShipper) Log(ev kail.Event) error {
+	if l.logglyClient != nil && ev != nil && len(ev.Log()) > 0 {
+		return l.logglyClient.Send(map[string]interface{}{
+			"message":   string(ev.Log()),
+			"node":      ev.Source().Node(),
+			"name":      ev.Source().Name(),
+			"namespace": ev.Source().Namespace(),
+			"container": ev.Source().Container(),
+		})
+	}
+	return nil
+}
+
+func (l *LogglyShipper) Close() error {
+	return nil
+}
---
+++ logshipper/papertrail.go
@@ -0,0 +1,49 @@
+package logshipper
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/boz/kail"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	papertrailgo "github.com/solarwinds/papertrail-go"
+)
+
+type PapertrailShipper struct {
+	papertrailShipperInst papertrailgo.LoggerInterface
+}
+
+func CreatePapertrailShipper(ctx context.Context, paperTrailProtocol, paperTrailHost string, paperTrailPort int,
+	dbLocation string, retention time.Duration,
+	workerCount int, maxDiskUsage float64) (*PapertrailShipper, error) {
+	lg, err := papertrailgo.NewPapertrailLogger(ctx, paperTrailProtocol, paperTrailHost, paperTrailPort, dbLocation, retention, workerCount, maxDiskUsage)
+	if err != nil {
+		err = errors.Wrap(err, "unable to create a papertrail logger")
+		logrus.Error(err)
+		return nil, err
+	}
+	return &PapertrailShipper{
+		papertrailShipperInst: lg,
+	}, nil
+}
+
+func (l *PapertrailShipper) Log(ev kail.Event) error {
+	if l.papertrailShipperInst != nil && ev != nil && len(ev.Log()) > 0 {
+		payload := &papertrailgo.Payload{
+			Hostname: ev.Source().Name(),
+			Tag:      fmt.Sprintf("node(%s)/namespace(%s)/container(%s)", ev.Source().Node(), ev.Source().Namespace(), ev.Source().Container()),
+			Log:      string(ev.Log()),
+		}
+		return l.papertrailShipperInst.Log(payload)
+	}
+	return nil
+}
+
+func (l *PapertrailShipper) Close() error {
+	if l.papertrailShipperInst != nil {
+		return l.papertrailShipperInst.Close()
+	}
+	return nil
+}
