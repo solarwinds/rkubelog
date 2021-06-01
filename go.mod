c68b1506b721a1868bf040a75b999fdf55ceb981
girishranganathan <31675149+girishranganathan@users.noreply.github.com>
2021-06-01 18:29:22 -0400

    - updated papertrail-go to support remote IP feature (#36)
    Co-authored-by: Girish Ranganathan <girish.ranganathan@solarwinds.com>

--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/segmentio/go-loggly v0.5.0
 	github.com/sirupsen/logrus v1.8.1
-	github.com/solarwinds/papertrail-go v0.0.0-20210309063915-0dce0e9dca34
+	github.com/solarwinds/papertrail-go v0.0.0-20210601025410-ab261ef9e67e
 	github.com/spf13/pflag v1.0.3 // indirect
 	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
 	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9 // indirect
--- vendor/github.com/solarwinds/papertrail-go/syslog.go
+++ vendor/github.com/solarwinds/papertrail-go/syslog.go
@@ -131,18 +131,28 @@ func (s *SrslogShipper) Write(packet *SyslogPacket) (err error) {
 		ts = time.Now()
 	}
 
-	timestamp := ts.Format(time.RFC3339)
-
 	// msg := fmt.Sprintf("<%d> %s %s %s %s - - - %s", packet.Severity, ts.Format(rfc5424time), packet.Hostname, s.tag, packet.Tag, packet.Message)
-	var msg string
+	// var msg string
 
 	tag := packet.Tag
 	if s.tag != "" {
-		tag = fmt.Sprintf("%s/%s", s.tag, tag)
+		tag = fmt.Sprintf("%s - %s", s.tag, tag)
 	}
 
-	msg = fmt.Sprintf("<%d>%d %s %s %s - - - %s",
-		packet.Severity, 1, timestamp, packet.Hostname, tag, packet.Message)
+	timestamp := ts.Format(time.RFC3339)
+	// // r19
+	// msg := fmt.Sprintf("<%d>%d %s %s %s - - - %s",
+	// 	packet.Severity, 1, timestamp, packet.Hostname, tag, packet.Message)
+
+	// r17
+	// msg = fmt.Sprintf("%s %s %s - %s", packet.Hostname, s.tag, packet.Tag, packet.Message)
+
+	// works, get IP
+	// msg := fmt.Sprintf("%s %s - - - %s", packet.Hostname, tag, packet.Message)
+
+	// msg := fmt.Sprintf("%s - %s - %s - %s", packet.Hostname, tag, timestamp, packet.Message)
+
+	msg := fmt.Sprintf("%s - %s - %s - %s", packet.Hostname, tag, timestamp, packet.Message)
 
 	_, err = s.writer.WriteWithPriority(packet.Severity, []byte(msg))
 	return err
